803f60358b2e9d6b981392b55d4388c223878402
JB Theard <jbtheard@gmail.com>
2022-01-11 10:50:26 +0000

    localised link

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,12 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+        {
+            resolve: `gatsby-theme-i18n`,
+            options: {
+              defaultLang: `en`,
+              configPath: require.resolve(`./i18n/config.json`),
+            },
+          },
     ]
 }
